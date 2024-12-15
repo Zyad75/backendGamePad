40ca3a4616906dfda9c1be2bdc7b348098b828b4
Zyad TAIBI <zyadt@MacBook-Air-de-Zyad.local>
2024-12-15 17:29:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,196 @@
+//serveur + database
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+//gestion du mdp et token
+const uid2 = require("uid2");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+
+// cors pour les requetes, app pour créer les différentes routes, use json pour les parametres body
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// variables d'environnement
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI);
+
+const User = mongoose.model("User", {
+  email: { type: String, unique: true },
+  username: String,
+  token: String,
+  hash: String,
+  salt: String,
+});
+//model favoris
+const Favorite = mongoose.model("Favorite", {
+  gameId: String,
+  title: String,
+  image: String,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+/// route pour s'inscrire
+app.post("/signup", async (req, res) => {
+  try {
+    console.log(req.body);
+    // ai-je tout ?
+    if (!req.body.username || !req.body.email || !req.body.password) {
+      return res.status(400).json({ error: "Missing parameters" });
+    }
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(409).json({ error: "email already used" });
+    }
+    // 1) salt et token
+    const salt = uid2(16);
+    const token = uid2(32);
+
+    // 2) hashing
+    const hash = SHA256(req.body.password + salt).toString(encBase64);
+
+    // 3) on enregistre le user
+
+    const newUser = new User({
+      email: req.body.email,
+      username: req.body.username,
+      token: token,
+      salt: salt,
+      hash: hash,
+    });
+
+    console.log(newUser);
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      token: newUser.token,
+      username: newUser.username,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    console.log(req.body);
+    // ai-je tout ?
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).json({ error: "Missing parameters" });
+    }
+    const userFound = await User.findOne({ email: req.body.email });
+
+    if (!userFound) {
+      return res.status(400).json({ error: "email incorect" });
+    }
+
+    const newHash = SHA256(req.body.password + userFound.salt).toString(
+      encBase64
+    );
+
+    if (userFound.hash !== newHash) {
+      return res.status(400).json({ error: "email/mot de passe incorect" });
+    }
+
+    const responseObject = {
+      _id: userFound._id,
+      token: userFound.token,
+      username: userFound.username,
+    };
+
+    res.json(responseObject);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//middleware authentification
+
+const isAuthenticated = async (req, res, next) => {
+  const token = req.headers.authorization.replace("Bearer ", "");
+
+  // a qui ce token correspond ?
+
+  const user = await User.findOne({ token: token }).select("-salt -hash");
+
+  if (!user) {
+    return res.status(401).json({ error: "unauthorized" });
+  }
+
+  req.user = user;
+  next();
+};
+//route pour ajouter un jeux en favoris
+app.post("/favorite", isAuthenticated, async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.image) {
+      return res.status(400).json({ error: "Missing parameters" });
+    }
+    const existingFav = await Favorite.findOne({
+      title: req.body.title,
+      owner: req.user._id,
+    });
+
+    if (existingFav) {
+      return res.status(409).json({ error: "Favorite already saved" });
+    }
+    const newFav = new Favorite({
+      gameId: req.body.gameId,
+      title: req.body.title,
+      image: req.body.image,
+      owner: req.user._id,
+    });
+
+    console.log(newFav);
+
+    await newFav.save();
+    res.status(200).json({ message: "Game added in Favorites" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// route en get pour avoir la liste des favoris liées à un compte user
+app.get("/favoritesOfUser", isAuthenticated, async (req, res) => {
+  try {
+    const favs = await Favorite.find({ owner: req.user._id });
+    if (!favs) {
+      return res.status(401).json({ error: "No Favorites" });
+    }
+
+    res.status(200).json(favs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+//Route pour supprimer un Favoris (je n'ai pas réussi à la faire en route delete, lorsque je met le middleware isAuthenticated
+// en route delete, ca ne fonctionne pas)
+app.post("/deleteFav", isAuthenticated, async (req, res) => {
+  try {
+    console.log(req.body.title);
+    console.log(req.user._id);
+    const favToDelete = await Favorite.deleteOne({
+      title: req.body.title,
+      owner: req.user._id,
+    });
+    res.status(200).json(favToDelete);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.all("*", (req, res) => {
+  res.json({ message: "all Routes" });
+});
+const PORT = 3000;
+app.listen(process.env.PORT || PORT, () => {
+  console.log("server GamePad Connected");
+});
